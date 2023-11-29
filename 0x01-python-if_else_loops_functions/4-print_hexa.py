654cd65aff71e37a5a0b3f20394af5017dbfc23e
MosheMatabologa <gideonmatabologa@gmail.com>
2023-11-28 17:25:20 -0800

    hey

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def function4():
-    for number in range(0, 98):
-        print("{} = {}".format(number, hex(number)), end=", ")
+    for number in range(0,99):
+        print("{} = {} = 0x{}".format(number, number, hex(number)))
 function4()
-
