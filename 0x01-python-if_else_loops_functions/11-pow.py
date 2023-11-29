0e40d622ce6c7dd02b5636c63da88d1d0e3e5bfd
MosheMatabologa <gideonmatabologa@gmail.com>
2023-11-28 17:58:37 -0800

    c2

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def pow(a, b):
-    return(a ** b)
-pow()
+def my_pow(a, b):
+    result = 1
+
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+
+    return result
+my_pow()
