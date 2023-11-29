0e58789f3e611853249a7e7203a0d1d07e23240d
MosheMatabologa <gideonmatabologa@gmail.com>
2023-11-28 17:04:11 -0800

    jsdjfsd

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def function4():
-    for number in range(0,99):
-        print("{:d} {:d}".format(number, hex(number)))
-
+    for i in range(99):
+        print("{:02d}".format(i), end=", ")
+            print("{:02d}".format(99))
 function4()
