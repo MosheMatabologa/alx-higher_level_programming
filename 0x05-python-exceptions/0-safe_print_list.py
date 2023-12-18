e9a63ef5ddbdf00fa43e688795dd68f2cf409319
MosheMatabologa <gideonmatabologa@gmail.com>
2023-12-18 11:04:26 -0800

    we are pushing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,4 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             printed_elements += 1
     except IndexError:
-        pass
-    finally:
-        print()
-
     return printed_elements
